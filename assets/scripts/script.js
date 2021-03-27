5ba716d38226e4984af6451f127374af545eb42b
William Lucht <aaronlucht@gmail.com>
2021-03-26 18:16:47 -0700

    started working on setting the Open Weather API token and console.log-ing the request

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -31,3 +31,28 @@
             // append to list in sidebar 
                 // list item should be clickable and runs the search for that city/input value
     
+// Start js
+var cityWeather = $("#city-weather"); // the output target for api city data
+var cityForecastr = $("#city-forecast"); // out target for 5 day forecast
+var apiToken = 'http://api.openweathermap.org/data/2.5/forecast?q=london,uk&appid=ce7ea9acf7f559c24dcf65e60fbcabe5'; //works only for London, UK at the moment
+console.log(apiToken);
+
+// var getUserRepos = function (user) {
+//     var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=london,uk&appid=ce7ea9acf7f559c24dcf65e60fbcabe5';
+  
+//     fetch(apiUrl)
+//       .then(function (response) {
+//         if (response.ok) {
+//           response.json().then(function (data) {
+//             displayRepos(data, user);
+//           });
+//         } else {
+//           alert('Error: ' + response.statusText);
+//         }
+//       })
+//       .catch(function (error) {
+//         alert('Unable to connect to Open Weather!');
+//       });
+//   };
+
+  
