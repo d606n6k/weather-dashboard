f6bb7c52177ac47e81dc17639920737d3c9c4f85
William Lucht <aaronlucht@gmail.com>
2021-03-29 21:04:30 -0700

    massive css rework/refinement. Restructured some html to meet deadline

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -29,7 +29,11 @@
 
 // Start js
 var cityWeather = $("#city-weather"); // the output target for api city data
-var cityForecaster = $("#city-forecast"); // out target for 5 day forecast data
+var cityForecaster1 = $("#city-forecast1"); // out target for 5 day forecast data
+var cityForecaster2 = $("#city-forecast2"); // out target for 5 day forecast data
+var cityForecaster3 = $("#city-forecast3"); // out target for 5 day forecast data
+var cityForecaster4 = $("#city-forecast4"); // out target for 5 day forecast data
+var cityForecaster5 = $("#city-forecast5"); // out target for 5 day forecast data
 var cityInput = $("#city-input");
 var submitBtn = $("#submit-btn");
 var searchListGroup = $(".list-group");
@@ -85,15 +89,15 @@ function renderFiveDay(weather){
     var fch2 = document.createElement("h2");
     fch2.classList = "p-2 rounded";
     fch2.textContent = "Five Day Forecast";
-    cityForecaster.append(fch2);    
+    cityForecaster1.append(fch2);    
 
-    // first item 
+    // first day 
     var dateData = weather.list[0].dt_txt;
     dateData.split(' ');
     var fcDateFirst = document.createElement("p");
-    fcDateFirst.classList = "p-2";
+    fcDateFirst.classList = "p-2 d-inline";
     fcDateFirst.textContent = dateData; 
-    cityForecaster.append(fcDateFirst);
+    cityForecaster1.append(fcDateFirst);
 
     // icon
     var fiveDayIcon = weather.list[0].weather[0].icon;
@@ -101,19 +105,133 @@ function renderFiveDay(weather){
     var fcIconFirst = document.createElement("img");
     fcIconFirst.classList = "p-2 ml-2 weather-icon rounded-circle";
     fcIconFirst.setAttribute("src", fiveDayIconUrl);
-    cityForecaster.append(fcIconFirst);
+    cityForecaster1.append(fcIconFirst);
 
     // temp
     var fcTempFirst = document.createElement("p");
     fcTempFirst.classList = "mb-2 mt-1 ml-2";
     fcTempFirst.textContent = "Projected Temperature: " +weather.list[0].main.temp + " °F"; 
-    cityForecaster.append(fcTempFirst);
+    cityForecaster1.append(fcTempFirst);
 
     // humidity
     var fcHumidityFirst = document.createElement("p");
     fcHumidityFirst.classList = "mb-2 mt-1 ml-2";
     fcHumidityFirst.textContent = "Projected Humidity: " + weather.list[0].main.humidity + "%"; 
-    cityForecaster.append(fcHumidityFirst);
+    cityForecaster1.append(fcHumidityFirst);
+
+    
+    // second day 
+    var dateData = weather.list[1].dt_txt;
+    dateData.split(' ');
+    var fcDateFirst = document.createElement("p");
+    fcDateFirst.classList = "p-2 d-inline";
+    fcDateFirst.textContent = dateData; 
+    cityForecaster2.append(fcDateFirst);
+
+    // icon
+    var fiveDayIcon = weather.list[1].weather[0].icon;
+    var fiveDayIconUrl = `HTTPS://openweathermap.org/img/wn/${fiveDayIcon}.png`;
+    var fcIconFirst = document.createElement("img");
+    fcIconFirst.classList = "p-2 ml-2 weather-icon rounded-circle";
+    fcIconFirst.setAttribute("src", fiveDayIconUrl);
+    cityForecaster2.append(fcIconFirst);
+
+    // temp
+    var fcTempFirst = document.createElement("p");
+    fcTempFirst.classList = "mb-2 mt-1 ml-2";
+    fcTempFirst.textContent = "Projected Temperature: " +weather.list[1].main.temp + " °F"; 
+    cityForecaster2.append(fcTempFirst);
+
+    // humidity
+    var fcHumidityFirst = document.createElement("p");
+    fcHumidityFirst.classList = "mb-2 mt-1 ml-2";
+    fcHumidityFirst.textContent = "Projected Humidity: " + weather.list[1].main.humidity + "%"; 
+    cityForecaster2.append(fcHumidityFirst);
+
+    
+    // third day 
+    var dateData = weather.list[2].dt_txt;
+    dateData.split(' ');
+    var fcDateFirst = document.createElement("p");
+    fcDateFirst.classList = "p-2 d-inline";
+    fcDateFirst.textContent = dateData; 
+    cityForecaster3.append(fcDateFirst);
+
+    // icon
+    var fiveDayIcon = weather.list[2].weather[0].icon;
+    var fiveDayIconUrl = `HTTPS://openweathermap.org/img/wn/${fiveDayIcon}.png`;
+    var fcIconFirst = document.createElement("img");
+    fcIconFirst.classList = "p-2 ml-2 weather-icon rounded-circle";
+    fcIconFirst.setAttribute("src", fiveDayIconUrl);
+    cityForecaster3.append(fcIconFirst);
+
+    // temp
+    var fcTempFirst = document.createElement("p");
+    fcTempFirst.classList = "mb-2 mt-1 ml-2";
+    fcTempFirst.textContent = "Projected Temperature: " +weather.list[2].main.temp + " °F"; 
+    cityForecaster3.append(fcTempFirst);
+
+    // humidity
+    var fcHumidityFirst = document.createElement("p");
+    fcHumidityFirst.classList = "mb-2 mt-1 ml-2";
+    fcHumidityFirst.textContent = "Projected Humidity: " + weather.list[2].main.humidity + "%"; 
+    cityForecaster3.append(fcHumidityFirst);
+
+    // fourth day 
+    var dateData = weather.list[2].dt_txt;
+    dateData.split(' ');
+    var fcDateFirst = document.createElement("p");
+    fcDateFirst.classList = "p-2 d-inline";
+    fcDateFirst.textContent = dateData; 
+    cityForecaster4.append(fcDateFirst);
+
+    // icon
+    var fiveDayIcon = weather.list[3].weather[0].icon;
+    var fiveDayIconUrl = `HTTPS://openweathermap.org/img/wn/${fiveDayIcon}.png`;
+    var fcIconFirst = document.createElement("img");
+    fcIconFirst.classList = "p-2 ml-2 weather-icon rounded-circle";
+    fcIconFirst.setAttribute("src", fiveDayIconUrl);
+    cityForecaster4.append(fcIconFirst);
+
+    // temp
+    var fcTempFirst = document.createElement("p");
+    fcTempFirst.classList = "mb-2 mt-1 ml-2";
+    fcTempFirst.textContent = "Projected Temperature: " +weather.list[3].main.temp + " °F"; 
+    cityForecaster4.append(fcTempFirst);
+
+    // humidity
+    var fcHumidityFirst = document.createElement("p");
+    fcHumidityFirst.classList = "mb-2 mt-1 ml-2";
+    fcHumidityFirst.textContent = "Projected Humidity: " + weather.list[3].main.humidity + "%"; 
+    cityForecaster4.append(fcHumidityFirst);
+
+    // fifth day
+    var dateData = weather.list[2].dt_txt;
+    dateData.split(' ');
+    var fcDateFirst = document.createElement("p");
+    fcDateFirst.classList = "p-2 d-inline";
+    fcDateFirst.textContent = dateData; 
+    cityForecaster5.append(fcDateFirst);
+
+    // icon
+    var fiveDayIcon = weather.list[4].weather[0].icon;
+    var fiveDayIconUrl = `HTTPS://openweathermap.org/img/wn/${fiveDayIcon}.png`;
+    var fcIconFirst = document.createElement("img");
+    fcIconFirst.classList = "p-2 ml-2 weather-icon rounded-circle";
+    fcIconFirst.setAttribute("src", fiveDayIconUrl);
+    cityForecaster5.append(fcIconFirst);
+
+    // temp
+    var fcTempFirst = document.createElement("p");
+    fcTempFirst.classList = "mb-2 mt-1 ml-2";
+    fcTempFirst.textContent = "Projected Temperature: " +weather.list[4].main.temp + " °F"; 
+    cityForecaster5.append(fcTempFirst);
+
+    // humidity
+    var fcHumidityFirst = document.createElement("p");
+    fcHumidityFirst.classList = "mb-2 mt-1 ml-2";
+    fcHumidityFirst.textContent = "Projected Humidity: " + weather.list[4].main.humidity + "%"; 
+    cityForecaster5.append(fcHumidityFirst);
 };
 
 // city current weather 
@@ -143,7 +261,7 @@ function fetchUvIndex(){
 
   // 5 day forecast
 function weatherFiveDay(getText){
-   var fiveDayApiToken = 'HTTPS://api.openweathermap.org/data/2.5/forecast?q=' + getText + '&mode=json&cnt=5&units=metric&cnt=5&appid=598c713e4a8e658a3dfa9b54d09a9d30';
+   var fiveDayApiToken = 'HTTPS://api.openweathermap.org/data/2.5/forecast?q=' + getText + '&mode=json&cnt=5&units=imperial&cnt=5&appid=598c713e4a8e658a3dfa9b54d09a9d30';
     fetch(fiveDayApiToken)
     .then(response => response.json())
     .then(data => renderFiveDay(data));
@@ -162,13 +280,17 @@ submitBtn.on("click", function(event){
         searchListGroup.empty();
         for(i = 0; i < searchedCities.length; i++){
             var searchedItem = document.createElement("button");
-            searchedItem.setAttribute("class", "list-group-item");
+            searchedItem.classList = "list-group-item mt-2 rounded";
             searchedItem.textContent = searchedCities[i];
             searchListGroup.append(searchedItem);
         }
         // searchedCities = [];
         cityWeather.empty();
-        cityForecaster.empty();
+        cityForecaster1.empty();
+        cityForecaster2.empty();
+        cityForecaster3.empty();
+        cityForecaster4.empty();
+        cityForecaster5.empty();
         fetchWeather(getText);
         weatherFiveDay(getText);
     }
@@ -176,13 +298,12 @@ submitBtn.on("click", function(event){
 
 // Not sure if this code is needed
 function getPastSearch(){
-    
     var getItems = JSON.parse(window.localStorage.getItem("city"));
     // console.log(getItems);
     for(i = 0; i < getItems.length; i++){
         // debugger;
         var searchedItem = document.createElement("button");
-        searchedItem.setAttribute("class", "list-group-item");
+        searchedItem.classList = "list-group-item mt-2 rounded";
         searchedItem.textContent = getItems[i];
         searchListGroup.append(searchedItem);
     }
